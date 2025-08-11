41dbfb185fd437161de1d2dcc2cf2dd23872e793
Max1973Iv <blgroup3758844@gmail.com>
2025-08-11 22:49:37 +0300

    реализ.первый этап регистрации пользователя.создание сессии.

--- classesP/autent.py
+++ classesP/autent.py
@@ -1,44 +1,35 @@
 # Класс - сервис управления регистрацией и аутентификацией пользователей
 from datetime import datetime
 class AuthenticationService:
-    def __init__(self):#,user_class:str, username: str, email: str, password: str,*args):
-        print(f'сессия начата:время начала сессии: {self.x_time()}')
+    # список созданных сессий
+    sessions = []
     #
-    #
+    def __init__(self):
         #
+        AuthenticationService.sessions.append(self) # Добавляем сессию в список открытых сессий
+        print(f"aut_Сессия {self.x_time()} начата.")
         """
         Инициализация сервиса аутентификации. экземпляр класса - сессия.
-        :param user_class: Класс пользователя (Customer или Admin)
-        :param username: Имя пользователя.
-        :param email: Email пользователя.
-        :param password: Пароль пользователя.
-        :param args: Дополнительные аргументы для создания пользователя.
         """
-#        if user_class not in ['Customer', 'Admin']:
-#            raise ValueError("Неверный класс пользователя. Доступны только 'Customer' и 'Admin'.")
-        #if not username or not email or not password:
-        #    raise ValueError("Имя пользователя, email и пароль не могут быть пустыми.")
 #
-#        self.user_class = user_class
-#        self.username = username
-#        self.email = email
-#        self.password = password
-#        self.args = args
+#    def __del__(self):
+#        print(f'сессия завершена:время окончания сессии: {self.x_time()}')
+#
     def x_time(self):
         """ Возвращает дату и время события.
         """
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #        
 # регистрация нового пользователя:
-#    @staticmethod
     def register(self,user_class:str, username: str, email: str, password: str,**kwargs):
-        self.user_class = user_class
-        self.username = username
-        self.email = email
-        self.password = password
-        self.args = kwargs
+        self.user_class = user_class # класс пользователя
+        self.username = username # имя пользователя
+        self.email = email # email пользователя
+        self.password = password # пароль пользователя
+        # Дополнительные аргументы для создания пользователя
+        self.kwargs = kwargs
         try:
-            if user_class not in ['Customer', 'Admin']:
+            if user_class not in ['Customer', 'Admin']: 
                 raise ValueError("Неверный класс пользователя. Доступны только 'Customer' и 'Admin'.")
             #
             try:
@@ -48,8 +39,8 @@ class AuthenticationService:
                 print(f"Ошибка регистрации: {e}")
                 return False, None
             #
-        except ValueError as e:
-            print(f"Ошибка регистрации: {e}")
+        except ValueError as ee:
+            print(f"Ошибка регистрации: {ee}")
             return False, None
 #
         print(f"aut_значение user_class {user_class}")
@@ -58,29 +49,16 @@ class AuthenticationService:
             try:
                 user_new = Customer(username=username, email=email, password=password, **kwargs)
                 return True, user_new
-            except ValueError as e:
-                print(f"aut_Ошибка создания пользователя: {e}")
+            except ValueError as ae:
+                print(f"aut_Ошибка создания пользователя: {ae}")
                 return False, None
         if user_class == "Admin":
             from classesP.users import Admin
             try:
                 user_new = Admin(username=username, email=email, password=password, **kwargs)
-#
-#                if  user_new == None:
-#                    raise ValueError("aut_Ошибка создания пользователя.")
-#                raise ValueError("aut_Ошибка создания пользователя.")
                 return True, user_new
-            except ValueError as e:
-                print(f"aut_Ошибка создания пользователя: {e}")
+            except ValueError as be:
+                print(f"aut_Ошибка создания пользователя: {be}")
                 return False, None
-#        print(f'globals: {globals()['Admin']}')
-        #from classes.users import Customer, Admin
-#        user_cl = globals()[user_class]  # Получаем ссылку на класс пользователя по имени
-        #
-        # Создаем нового пользователя
-        #print(f"Регистрация нового пользователя: {user_cl} с именем {username}, email {email}, пароль {password}")
-        #print("значение user_class",user_cl)
-#        user_new = user_cl(username=username, email=email, password=password, **kwargs)#создаем экземпляр класса пользователя
-        #print(f"Пользователь {user_new.username} успешно зарегистрирован.")
-        #return True, user_new
+#
         #
--- shop.py
+++ shop.py
@@ -18,24 +18,20 @@ customer2 = Customer(username="Вася", email="aaa@jhf.bu", password = "12345q
 # Выводим список всех пользователей
 print("Список всех пользователей:")
 print(Admin.get_all_users()) # Выводим список всех пользователей
-## Регистрация нового пользователя через сервис аутентификации
-auth_service_inst = AuthenticationService()
-# Регистрация нового пользователя
 #
-auth_service = auth_service_inst.register(user_class="Customer", username="Жужик", email="jhf@by", password="0986667qwerty", address="Минск, ул. Советская, 3")
-#auth_service = auth_service_inst.register(user_class="Customer",username="Жора",email="hgf5@8jh.by", password="0987qwerty", address="Минск, ул. Советская, 3")
-#print(f"Регистрация нового пользователя:{auth_service_inst.username} {auth_service}")
-if auth_service[0]==True:
-    print(f"shop_Пользователь {auth_service[1].username} успешно зарегистрирован.")
-    print(f"shop_Детали нового пользователя: {auth_service[1].get_details()}")
-else:
-    print("shop_Регистрация не удалась.")
-#print(f"Регистрация нового пользователя: {auth_service[0]}")
-#print(f"Детали нового пользователя: {auth_service[1].get_details()}")
+# Регистрация нового пользователя через сервис аутентификации
+auth_service_inst = AuthenticationService() # Создаем экземпляр сервиса аутентификации -сессия
+# Регистрация нового пользователя
+#auth_service = auth_service_inst.register(user_class="Customer", username="Василий", email="jhf@by", password="0986667qwerty", address="Минск, ул. Советская, 3")
+auth_service = auth_service_inst.register(user_class="Admin",username="Жора_fadmin",email="hgf5@8j22h.by", password="098777qwerty", admin_level=4)
+#
+#print(auth_service)
+#
 # Выводим список всех пользователей после регистрации нового пользователя
 print("Список всех пользователей после регистрации нового пользователя:")
 print(Admin.get_all_users()) # Выводим список всех пользователей
-#print("Список всех пользователей2:")
-#print(Admin.get_all_users()) # Выводим список всех пользователей
-# время и дата создания
-#print(f"Дата и время создания: {AuthenticationService.creation_time()}")
+#
+# вывести список открытых сессий
+print("Список открытых сессий:")
+for session in AuthenticationService.sessions:
+    print(f"Сессия начата: {session.x_time()}")  # Выводим время начала каждой сессии
